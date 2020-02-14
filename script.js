97c6a1dd9de009ddff31cc149c77d6aabd4175ae
Tom <example@gmail.com>
2020-02-14 20:20:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+
+// let persone = {
+//   name:"Jhon",
+//   age:25,
+//   isMarried: false
+// };
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr[0]);
+
+// alert('Hello,world');
+
+// let answer = confirm('Are you gey?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+// console.log(4 + +" - object");
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" ===2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
